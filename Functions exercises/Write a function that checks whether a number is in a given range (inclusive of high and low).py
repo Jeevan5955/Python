ca67dc55771e06798f675ed96f61f79407d780f1
Jeevan N <54932235+Jeevan5955@users.noreply.github.com>
2020-07-14 17:19:16 +0530

    Create Write a function that checks whether a number is in a given range (inclusive of high and low).py

---
+++ Functions exercises/Write a function that checks whether a number is in a given range (inclusive of high and low).py
@@ -0,0 +1,6 @@
+def ran_check(num,low,high):
+    #Check if num is between low and high (including low and high)
+    if num in range(low,high+1):
+        print('{} is in the range between {} and {}'.format(num,low,high))
+    else:
+        print('The number is outside the range.')
